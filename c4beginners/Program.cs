1a8d2c84800dfd1e7edb63fb1b3e3b1cd301d034
Divyang Mehta <dmehta@mail.usf.edu>
2020-02-08 17:17:22 -0500

    Add project files.

---
+++ c4beginners/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace c4beginners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am starting to write in C#.");
+            Console.ReadLine();
+        }
+    }
+}
